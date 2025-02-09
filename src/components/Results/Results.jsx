24347a556245e83cc228dff28b57825110e0be38
Aleksandra Wilk <alex.dev.wilk@gmail.com>
2025-02-09 11:43:20 +0100

    add table to result

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import { calculateInvestmentResults } from '../../utils/calculateInvestmentResults';
 import './Results.scss';
+import { formatter } from '../../utils/formatter';
 
 export const Results = ({ input }) => {
   const resultData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
 
-  console.log(resultData);
-  return <div id='result'>Results</div>;
+  return (
+    <table id='result'>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest </th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map((yearData) => (
+          <tr key={yearData.year}>
+            <td>{yearData.year}</td>
+            <td>{formatter.format(yearData.valueEndOfYear)}</td>
+            <td>{formatter.format(yearData.interest)}</td>
+            <td>
+              {formatter.format(
+                yearData.valueEndOfYear -
+                  yearData.annualInvestment * yearData.year -
+                  initialInvestment
+              )}
+            </td>
+            <td>
+              {formatter.format(
+                yearData.valueEndOfYear -
+                  (yearData.valueEndOfYear -
+                    yearData.annualInvestment * yearData.year -
+                    initialInvestment)
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
